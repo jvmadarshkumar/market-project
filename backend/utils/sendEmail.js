d724bcabcd397bbf75fd2487735444a6684b6f4b
Adarsh Kumar <jvmadarshkumar@gmail.com>
2026-05-05 14:34:26 +0530

    fix: force IPv4 DNS resolution for nodemailer

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,5 +1,9 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
 
+// Force Node.js to use IPv4. Render's free tier often fails to route IPv6 traffic,
+// causing ENETUNREACH errors when connecting to smtp.gmail.com.
+dns.setDefaultResultOrder('ipv4first');
 const sendEmail = async (options) => {
   let transporter;
 
